83a12b93c9db5c5b4f95a66286ec3e6d7318dd68
amirjahan1 <amirjahan454@gmail.com>
2022-08-05 16:34:07 +0430

    test rest

---
+++ main.py
@@ -0,0 +1,13 @@
+# import requests
+# dataForSend = {
+#       "id": 4,
+#       "first_name": "Amir",
+#       "last_name": "Jahan",
+#       "email": "amirjahan454@gmail.com"
+#     }
+# getAll = requests.get('http://localhost:3000/employees/1')
+# create = requests.post('http://localhost:3000/employees', data=dataForSend)
+# update = requests.put('http://localhost:3000/employees/4', data=dataForSend)
+# delete = requests.delete('http://localhost:3000/employees/1')
+# print(getAll.json(), create, update, delete)
+
